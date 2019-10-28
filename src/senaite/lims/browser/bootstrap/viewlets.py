718e4466f7bfbf297454e4c6421d619c2e00f770
Ramon Bartl <rb@ridingbytes.com>
2019-10-29 00:17:15 +0100

    Moved viewlets to core

--- src/senaite/lims/browser/bootstrap/viewlets.py
+++ src/senaite/lims/browser/bootstrap/viewlets.py
@@ -18,27 +18,9 @@
 # Copyright 2018-2019 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
-from cgi import escape
-
-from plone.app.layout.viewlets.common import GlobalSectionsViewlet
 from plone.app.layout.viewlets.common import SiteActionsViewlet
 from plone.app.layout.viewlets.content import DocumentActionsViewlet
-from Products.CMFPlone.utils import safe_unicode
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.component import getMultiAdapter
-
-
-class SenaiteSectionsDropdownViewlet(GlobalSectionsViewlet):
-    index = ViewPageTemplateFile(
-        'templates/senaite.lims.browser.bootstrap.viewlets.sections_dropdown.pt')
-
-    def update(self):
-        super(SenaiteSectionsDropdownViewlet, self).update()
-        portal_state = getMultiAdapter((self.context, self.request),
-                                       name=u'plone_portal_state')
-        self.navigation_root_url = portal_state.navigation_root_url()
-        self.portal_title = escape(
-            safe_unicode(portal_state.navigation_root_title()))
 
 
 class SenaiteSiteActionsViewlet(SiteActionsViewlet):
