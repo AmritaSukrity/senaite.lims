9d0e7ea3fd830f94d7064f5c175fb53b372ae68a
Ramon Bartl <rb@ridingbytes.com>
2019-10-28 23:55:19 +0100

    Removed viewlets view

--- src/senaite/lims/browser/bootstrap/viewlets.py
+++ src/senaite/lims/browser/bootstrap/viewlets.py
@@ -20,19 +20,12 @@
 
 from cgi import escape
 
-from Acquisition import aq_inner
-from plone.app.customerize import registration
-from plone.app.layout.viewlets.common import ContentViewsViewlet
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet
 from plone.app.layout.viewlets.common import SiteActionsViewlet
 from plone.app.layout.viewlets.content import DocumentActionsViewlet
 from Products.CMFPlone.utils import safe_unicode
-from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zExceptions import NotFound
 from zope.component import getMultiAdapter
-from zope.publisher.interfaces.browser import IBrowserRequest
-from zope.viewlet.interfaces import IViewlet
 
 
 class SenaiteSectionsDropdownViewlet(GlobalSectionsViewlet):
@@ -47,10 +40,6 @@ class SenaiteSectionsDropdownViewlet(GlobalSectionsViewlet):
         self.portal_title = escape(
             safe_unicode(portal_state.navigation_root_title()))
 
-class SenaiteContentViewsViewlet(ContentViewsViewlet):
-    index = ViewPageTemplateFile(
-        'templates/plone.app.layout.viewlets.contentviews.pt')
-
 
 class SenaiteSiteActionsViewlet(SiteActionsViewlet):
     index = ViewPageTemplateFile(
@@ -60,81 +49,3 @@ class SenaiteSiteActionsViewlet(SiteActionsViewlet):
 class SenaiteDocumentActionsViewlet(DocumentActionsViewlet):
     index = ViewPageTemplateFile(
         'templates/plone.app.layout.viewlets.documentactions.pt')
-
-
-class ViewletView(BrowserView):
-    """Expose arbitrary viewlets to traversing by name.
-
-    Example how to render plone.logo viewlet in arbitrary template code point::
-
-        <div tal:content="context/@@viewlets/plone.logo" />
-
-    https://docs.plone.org/develop/plone/views/viewlets.html#rendering-viewlet-by-name
-    """
-
-    def __init__(self, context, request):
-        super(ViewletView, self).__init__(context, request)
-        self.context = context
-        self.request = request
-
-    def getViewletByName(self, name):
-
-        """ Viewlets allow through-the-web customizations.
-
-        Through-the-web customization magic is managed by five.customerize.
-        We need to think of this when looking up viewlets.
-
-        @return: Viewlet registration object
-        """
-        views = registration.getViews(IBrowserRequest)
-
-        for v in views:
-
-            if v.provided == IViewlet:
-                # Note that we might have conflicting BrowserView with the same
-                # name, thus we need to check for provided
-                if v.name == name:
-                    return v
-
-        return None
-
-    def setupViewletByName(self, name):
-        """ Constructs a viewlet instance by its name.
-
-        Viewlet update() and render() method are not called.
-
-        @return: Viewlet instance of None if viewlet with name does not exist
-        """
-        context = aq_inner(self.context)
-        request = self.request
-
-        # Perform viewlet regisration look-up
-        # from adapters registry
-        reg = self.getViewletByName(name)
-        if reg is None:
-            return None
-
-        # factory method is responsible for creating the viewlet instance
-        factory = reg.factory
-
-        # Create viewlet and put it to the acquisition chain
-        # Viewlet need initialization parameters: context, request, view
-        try:
-            viewlet = factory(context, request, self, None).__of__(context)
-        except TypeError:
-            # Bad constructor call parameters
-            raise RuntimeError(
-                "Unable to initialize viewlet {}. "
-                "Factory method {} call failed."
-                .format(name, str(factory)))
-
-        return viewlet
-
-    def __getitem__(self, name):
-        """Allow travering intoviewlets by viewlet name.
-        """
-        viewlet = self.setupViewletByName(name)
-        if viewlet is None:
-            raise NotFound("Viewlet {} not found".format(name))
-        viewlet.update()
-        return viewlet.render()
